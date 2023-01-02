16d22e7bf80fa78b0aac984f7ea9fcdce3dd5254
zunema <yxxa.abc@gmail.com>
2023-01-03 00:36:30 +0900

    diaryControlleにindexを追加

--- src/main/java/com/example/demo/controller/DiaryController.java
+++ src/main/java/com/example/demo/controller/DiaryController.java
@@ -1,9 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Diary;
+import com.example.demo.service.DiaryService;
 
 @Controller
+@RequestMapping("/diary")
 public class DiaryController {
 
-    
+    @Autowired
+    DiaryService diaryService;
+
+    public String index(Model model){
+        
+        List<Diary> diary = diaryService.diaryAll();
+        model.addAttribute("diaryList", diary);
+
+        return "diary/index";
+    }
 }
