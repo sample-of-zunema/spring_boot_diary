7c014aaf1fb6386714ec0db52ef7005f3c8d7723
zunema <yxxa.abc@gmail.com>
2023-01-03 23:31:08 +0900

    fix

--- src/main/java/com/example/demo/controller/DiaryController.java
+++ src/main/java/com/example/demo/controller/DiaryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entity.Diary;
 import com.example.demo.service.DiaryService;
@@ -17,7 +18,7 @@ public class DiaryController {
     @Autowired
     DiaryService diaryService;
 
-    @RequestMapping("/index")
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(Model model) {
 
         List<Diary> diary = diaryService.diaryAll();
--- src/main/java/com/example/demo/service/DiaryServiceImpl.java
+++ src/main/java/com/example/demo/service/DiaryServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Diary;
 import com.example.demo.repository.DiaryRepository;
 
+@Service
 public class DiaryServiceImpl implements DiaryService {
 
     @Autowired
