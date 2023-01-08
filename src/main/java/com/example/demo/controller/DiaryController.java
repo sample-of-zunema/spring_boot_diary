841861491ada36b5afe0113b281d744eec3bd68d
zunema <yxxa.abc@gmail.com>
2023-01-08 21:05:51 +0900

    削除メソッド追加

--- src/main/java/com/example/demo/controller/DiaryController.java
+++ src/main/java/com/example/demo/controller/DiaryController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Diary;
+import com.example.demo.repository.DiaryRepository;
 import com.example.demo.service.DiaryService;
 
 @Controller
@@ -18,12 +22,30 @@ public class DiaryController {
     @Autowired
     DiaryService diaryService;
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @Autowired
+    DiaryRepository diaryRepository;
+
+    // 一覧取得
+    @RequestMapping("/index")
+    // @GetMapping("index")
     public String index(Model model) {
 
         List<Diary> diary = diaryService.diaryAll();
         model.addAttribute("diaryList", diary);
 
         return "diary/index";
+    }
+
+    // 削除
+    // @RequestMapping("/delete")
+    // public String delete(@RequestParam Integer id){
+    //     diaryService.delete(id);
+    // }
+
+    //指定されたidの日記を削除する
+    @PostMapping("delete")
+    public String delete(@RequestParam String id) {
+        diaryRepository.deleteById(id);
+        return "redirect:/diary/summary";
     }
 }
