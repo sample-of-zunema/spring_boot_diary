ae0d29403a91730e630aaa7f24179aecc4bc263e
zunema <yxxa.abc@gmail.com>
2023-01-09 16:15:20 +0900

    formクラス作成

---
+++ src/main/java/com/example/demo/form/diaryForm.java
@@ -0,0 +1,10 @@
+package com.example.demo.form;
+
+import java.time.LocalDateTime;
+
+public class diaryForm {
+    
+    private Integer id;
+    private String bodytext;
+    private LocalDateTime creDateTime;
+}
